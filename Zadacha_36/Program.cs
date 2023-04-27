712b718bbc783b174b32a62266fb905754f920b6
Vasilij U <vasya-1001@yandex.ru>
2023-04-27 21:40:11 +0500

    Zadacha 36 completed

--- Zadacha_36/Program.cs
+++ Zadacha_36/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int [] CreateArray(int Length, int min, int max)
+{
+    int [] numbers = new int [Length];
+    for (int i = 0; i<numbers.Length; i++)
+    {
+        numbers[i]=new Random().Next(min,max+1);
+    }
+    return numbers;
+}
+
+void PrintArray(int [] numbers)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        System.Console.Write(numbers[i]+ " ");
+    }
+    System.Console.WriteLine(" ]");
+}
+
+int Num (string arg)
+{
+    System.Console.WriteLine($"Введите {arg} ");
+    return int.Parse(System.Console.ReadLine());
+}
+
+void SumNechet (int [] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (!(i%2==0)) sum = sum + array[i];
+    }
+    System.Console.WriteLine(sum);
+}
+
+int a = Num("Enter array Length: ");
+int b = Num("Enter min value for random numbers: ");
+int c = Num("Enter max value for random numbers: ");
+
+int [] arr = CreateArray(a,b,c);
+PrintArray(arr);
+SumNechet(arr);
