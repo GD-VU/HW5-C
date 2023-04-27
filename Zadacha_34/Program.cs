2eee9fb5dfba78590b28a1a50cb8f899dea9bd23
Vasilij U <vasya-1001@yandex.ru>
2023-04-27 21:31:54 +0500

    Zadacha 34 completed

--- Zadacha_34/Program.cs
+++ Zadacha_34/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int [] CreateArray(int Length, int min, int max)
+{
+    int [] numbers = new int [Length];
+    for (int i = 0; i<numbers.Length; i++)
+    {
+        numbers[i]=new Random().Next(min,max+1);
+    }
+    return numbers;
+}
+
+void PrintArray(int [] numbers)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        System.Console.Write(numbers[i]+ " ");
+    }
+    System.Console.WriteLine(" ]");
+}
+
+int Num (string arg)
+{
+    System.Console.WriteLine($"Введите {arg} ");
+    return int.Parse(System.Console.ReadLine());
+}
+
+void GetCount (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2==0) count = count +1;
+    }
+    System.Console.WriteLine(count);
+}
+int a = Num("Enter array Length: ");
+int b = Num("Enter min value for random numbers: ");
+int c = Num("Enter max value for random numbers: ");
+
+int [] arr = CreateArray(a,b,c);
+PrintArray(arr);
+GetCount(arr);
