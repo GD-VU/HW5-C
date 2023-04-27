565374d6e822099fb883f6dc065f09d29ac47711
Vasilij U <vasya-1001@yandex.ru>
2023-04-27 21:50:05 +0500

    Zadacha 38 completed

--- Zadahca_38/Program.cs
+++ Zadahca_38/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int [] CreateArray(int Length, int min, int max)
+{
+    int [] numbers = new int [Length];
+    for (int i = 0; i<numbers.Length; i++)
+    {
+        numbers[i]=new Random().Next(min,max+1);
+    }
+    return numbers;
+}
+
+void PrintArray(int [] numbers)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        System.Console.Write(numbers[i]+ " ");
+    }
+    System.Console.WriteLine(" ]");
+}
+
+int Num (string arg)
+{
+    System.Console.WriteLine($"Введите {arg} ");
+    return int.Parse(System.Console.ReadLine());
+}
+
+void GetMaxAndMin (int [] array)
+{
+    int max = array[0];
+    int min = array[0];
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>max) max = array[i];
+        else if (array[i]<min) min = array[i];
+        // else System.Console.WriteLine("oops?");
+    }
+    sum=max-min;
+    System.Console.WriteLine($"Разность между максимальным чилом ({max}) и минимальным ({min}) равна {sum}.");
+}
+
+int a = Num("Enter array Length: ");
+int b = Num("Enter min value for random numbers: ");
+int c = Num("Enter max value for random numbers: ");
+
+int [] arr = CreateArray(a,b,c);
+PrintArray(arr);
+GetMaxAndMin(arr);
